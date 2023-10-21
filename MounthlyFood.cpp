35156fbb28d82ed6a121f086ce3572009cd035bc
Cheprichate <petrovich15071998@mail.ru>
2023-10-21 20:36:22 +0400

    Добавьте файлы проекта.

---
+++ MounthlyFood.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int mounth = 1;
+int food = 100;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    for (food != 0; food -= 4; mounth++) {
+
+        std::cout << "После " << mounth << " месяца у вас в запасе останется " << food << " кг гречки \n";
+    }
+    std::cout << "После " << mounth << " месяца гречка закончится \n";
+}
